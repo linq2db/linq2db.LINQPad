c002c982484e033855427688fd0b02046d5ce47d
Igor Tkachev <igor.tkachev@gmail.com>
2016-03-07 21:37:29 -0500

    Initial commit.

---
+++ LinqToDBDriver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using LINQPad.Extensibility.DataContext;
+
+namespace linq2db.LINQPad.Driver
+{
+	public class LinqToDBDriver : DynamicDataContextDriver
+	{
+		public override string Name   => "LINQ to DB";
+		public override string Author => "Igor Tkachev";
+
+		public override string GetConnectionDescription(IConnectionInfo cxInfo)
+		{
+			return ".......";
+		}
+
+		public override bool ShowConnectionDialog(IConnectionInfo cxInfo, bool isNewConnection)
+		{
+			return false;
+		}
+
+		public override List<ExplorerItem> GetSchemaAndBuildAssembly(
+			IConnectionInfo cxInfo, AssemblyName assemblyToBuild, ref string nameSpace, ref string typeName)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
