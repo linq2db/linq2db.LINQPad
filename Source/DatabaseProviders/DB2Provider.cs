4a7ae5d41cad534f7f9e3eff39f412348aae1d69
Marcus Hellsten <marcus.hellsten@claytonhomes.com>
2024-10-01 11:25:01 -0400

    Re-enable DB2 for iSeries support

--- Source/DatabaseProviders/DB2Provider.cs
+++ Source/DatabaseProviders/DB2Provider.cs
@@ -1,5 +1,7 @@
 ﻿using LinqToDB.Data;
 using System.Data.Common;
+using LinqToDB.DataProvider.DB2iSeries;
+
 #if NETFRAMEWORK
 using IBM.Data.DB2;
 #else
@@ -15,13 +17,13 @@ internal sealed class DB2Provider : DatabaseProviderBase
 		new (ProviderName.DB2LUW       , "DB2 for Linux, UNIX and Windows (LUW)"),
 		// zOS provider not tested at all as we don't have access to database instance
 		new (ProviderName.DB2zOS       , "DB2 for z/OS"                         ),
-		//new (DB2iSeriesProviderName.DB2, "DB2 for i (iSeries)"                  ),
+		new (DB2iSeriesProviderName.DB2, "DB2 for i (iSeries)"                  ),
 	];
 
 	public DB2Provider()
-		//: base(ProviderName.DB2, "IBM DB2 (LUW, z/OS or iSeries)", _providers)
-		: base(ProviderName.DB2, "IBM DB2 (LUW or z/OS)", _providers)
+		: base(ProviderName.DB2, "IBM DB2 (LUW, z/OS or iSeries)", _providers)
 	{
+		DataConnection.AddProviderDetector(DB2iSeriesTools.ProviderDetector);
 	}
 
 	public override void ClearAllPools(string providerName)
@@ -35,7 +37,7 @@ internal sealed class DB2Provider : DatabaseProviderBase
 		{
 			ProviderName.DB2LUW        => "SELECT MAX(TIME) FROM (SELECT MAX(ALTER_TIME) AS TIME FROM SYSCAT.ROUTINES UNION SELECT MAX(ALTER_TIME) AS TIME FROM SYSCAT.TABLES)",
 			ProviderName.DB2zOS        => "SELECT MAX(TIME) FROM (SELECT MAX(ALTEREDTS) AS TIME FROM SYSIBM.SYSROUTINES UNION SELECT MAX(ALTEREDTS) AS TIME FROM SYSIBM.SYSTABLES)",
-			//DB2iSeriesProviderName.DB2 => "SELECT MAX(TIME) FROM (SELECT MAX(LAST_ALTERED) AS TIME FROM QSYS2.SYSROUTINES UNION SELECT MAX(ROUTINE_CREATED) AS TIME FROM QSYS2.SYSROUTINES UNION SELECT MAX(LAST_ALTERED_TIMESTAMP) AS TIME FROM QSYS2.SYSTABLES)",
+			DB2iSeriesProviderName.DB2 => "SELECT MAX(TIME) FROM (SELECT MAX(LAST_ALTERED) AS TIME FROM QSYS2.SYSROUTINES UNION SELECT MAX(ROUTINE_CREATED) AS TIME FROM QSYS2.SYSROUTINES UNION SELECT MAX(LAST_ALTERED_TIMESTAMP) AS TIME FROM QSYS2.SYSTABLES)",
 			_                          => throw new LinqToDBLinqPadException($"Unknown DB2 provider '{settings.Connection.Provider}'")
 		};
 
